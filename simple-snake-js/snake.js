40c17978a3acb7c9a1961f1afccf85794c4e146f
Nick Thorpe <57202558+nickthorpe71@users.noreply.github.com>
2020-05-06 07:27:02 -0500

    Added Style Sheets
    - Header
    - Background Color
    - Snake Color

--- simple-snake-js/snake.js
+++ simple-snake-js/snake.js
@@ -8,7 +8,7 @@ function Snake() {
 
 
     this.draw = function () {
-        ctx.fillStyle = "#FFFFFF";
+        ctx.fillStyle = "#EEE0CB";
 
         for (let i = 0; i < this.tail.length; i++) {
             ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
